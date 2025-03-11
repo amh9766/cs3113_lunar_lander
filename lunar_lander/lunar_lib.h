4670071884a0f8686b9a78916d30c02ffcbe20f7
amh9766 <amh9766@nyu.edu>
2025-03-10 22:47:55 -0400

    feat!: Write `Entity` Class, Adjust Ortho Matrix

---
+++ lunar_lander/lunar_lib.h
@@ -0,0 +1,18 @@
+/**
+* Author: Amani Hernandez 
+* Assignment: Lunar Lander
+* Date due: 2025-3-15, 11:59pm
+* I pledge that I have completed this assignment without
+* collaborating with anyone else, in conformance with the
+* NYU School of Engineering Policies and Procedures on
+* Academic Misconduct.
+**/
+#ifndef LUNAR_LIB_H
+#define LUNAR_LIB_H
+
+constexpr float ACCEL_OF_GRAVITY = 2.5f,
+                ACCEL_OF_PROPULSION = 15.0f,
+                DRAG_COEFFICIENT = 0.25f;
+
+#endif
+
---
+++ lunar_lander/main.cpp
@@ -0,0 +1,284 @@
+/**
+* Author: Amani Hernandez 
+* Assignment: Lunar Lander
+* Date due: 2025-3-15, 11:59pm
+* I pledge that I have completed this assignment without
+* collaborating with anyone else, in conformance with the
+* NYU School of Engineering Policies and Procedures on
+* Academic Misconduct.
+**/
+#define STB_IMAGE_IMPLEMENTATION
+#define GL_SILENCE_DEPRECATION
+#define GL_GLEXT_PROTOTYPES 1
+
+#ifdef _WINDOWS
+    #include <GL/glew.h>
+#endif
+
+#include <SDL.h>
+#include <SDL_opengl.h>
+#include "glm/mat4x4.hpp"
+#include "glm/gtc/matrix_transform.hpp"
+#include "ShaderProgram.h"
+#include "stb_image.h"
+#include "cmath"
+#include <ctime>
+#include <vector>
+#include "Entity.h"
+#include "helper.h"
+
+// ————— CONSTANTS ————— //
+constexpr float INTERNAL_WIDTH  = 240.0f,
+                INTERNAL_HEIGHT = 160.0f;
+
+constexpr int WINDOW_WIDTH  = 960,
+              WINDOW_HEIGHT = 640;
+
+constexpr float BG_RED = 0.1922f,
+                BG_BLUE = 0.549f,
+                BG_GREEN = 0.9059f,
+                BG_OPACITY = 1.0f;
+
+constexpr int VIEWPORT_X = 0,
+              VIEWPORT_Y = 0,
+              VIEWPORT_WIDTH  = WINDOW_WIDTH,
+              VIEWPORT_HEIGHT = WINDOW_HEIGHT;
+
+constexpr char V_SHADER_PATH[] = "shaders/vertex_textured.glsl",
+               F_SHADER_PATH[] = "shaders/fragment_textured.glsl";
+
+constexpr float MILLISECONDS_IN_SECOND = 1000.0;
+constexpr char  PLAYER_FILEPATH[] = "content/player.png";
+
+constexpr GLint NUMBER_OF_TEXTURES = 1;
+constexpr GLint LEVEL_OF_DETAIL    = 0;
+constexpr GLint TEXTURE_BORDER     = 0;
+
+constexpr float FIXED_TIMESTEP = 1.0f / 60.0f;
+constexpr float ACC_OF_GRAVITY = -9.81f;
+
+// ————— STRUCTS AND ENUMS —————//
+enum AppStatus { RUNNING, TERMINATED };
+
+struct GameState
+{
+    Entity* player;
+};
+
+// ————— VARIABLES ————— //
+GameState g_game_state;
+
+SDL_Window* g_display_window;
+AppStatus g_app_status = RUNNING;
+
+ShaderProgram g_shader_program = ShaderProgram();
+glm::mat4 g_view_matrix, g_projection_matrix;
+
+float g_previous_ticks   = 0.0f;
+float g_time_accumulator = 0.0f;
+
+// ———— GENERAL FUNCTIONS ———— //
+GLuint load_texture(const char* filepath);
+
+void initialise();
+void process_input();
+void update();
+void render();
+void shutdown();
+
+GLuint load_texture(const char* filepath)
+{
+    int width, height, number_of_components;
+    unsigned char* image = stbi_load(filepath, &width, &height, &number_of_components, 
+                                     STBI_rgb_alpha);
+
+    if (image == NULL)
+    {
+        LOG("Unable to load image. Make sure the path is correct.");
+        assert(false);
+    }
+
+    GLuint textureID;
+    glGenTextures(NUMBER_OF_TEXTURES, &textureID);
+    glBindTexture(GL_TEXTURE_2D, textureID);
+    glTexImage2D(GL_TEXTURE_2D, LEVEL_OF_DETAIL, GL_RGBA, width, height, TEXTURE_BORDER, 
+                 GL_RGBA, GL_UNSIGNED_BYTE, image);
+
+    glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_MIN_FILTER, GL_NEAREST);
+    glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_MAG_FILTER, GL_NEAREST);
+
+    glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_WRAP_S, GL_REPEAT);
+    glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_WRAP_T, GL_REPEAT);
+
+    stbi_image_free(image);
+
+    return textureID;
+}
+
+void initialise()
+{
+    SDL_Init(SDL_INIT_VIDEO);
+    g_display_window = SDL_CreateWindow("Lunar Lander",
+        SDL_WINDOWPOS_CENTERED, SDL_WINDOWPOS_CENTERED,
+        WINDOW_WIDTH, WINDOW_HEIGHT,
+        SDL_WINDOW_OPENGL);
+
+    SDL_GLContext context = SDL_GL_CreateContext(g_display_window);
+    SDL_GL_MakeCurrent(g_display_window, context);
+    
+    if (context == nullptr)
+    {
+        LOG("ERROR: Could not create OpenGL context.\n");
+        shutdown();
+    }
+
+#ifdef _WINDOWS
+    glewInit();
+#endif
+
+    glViewport(VIEWPORT_X, VIEWPORT_Y, VIEWPORT_WIDTH, VIEWPORT_HEIGHT);
+
+    g_shader_program.load(V_SHADER_PATH, F_SHADER_PATH);
+
+    g_view_matrix       = IDENTITY_MAT;
+    g_projection_matrix = glm::ortho(0.0f, INTERNAL_WIDTH, INTERNAL_HEIGHT, 0.0f, -1.0f, 1.0f);
+
+    g_shader_program.set_projection_matrix(g_projection_matrix);
+    g_shader_program.set_view_matrix(g_view_matrix);
+
+    glUseProgram(g_shader_program.get_program_id());
+
+    glClearColor(BG_RED, BG_BLUE, BG_GREEN, BG_OPACITY);
+
+    // ————— PLAYER ————— //
+    g_game_state.player = new Entity(
+        glm::vec3(120.0f, 80.0f, 0.0f),
+        glm::vec3(24.0f, 19.0f, 0.0f),
+        24,
+        19,
+        load_texture(PLAYER_FILEPATH)   
+    );
+    
+    // ————— PLATFORM ————— //
+
+    // ————— GENERAL ————— //
+    glEnable(GL_BLEND);
+    glBlendFunc(GL_SRC_ALPHA, GL_ONE_MINUS_SRC_ALPHA);
+}
+
+void process_input()
+{
+    glm::vec3 applied_accel = glm::vec3(0.0f);
+
+    SDL_Event event;
+    while (SDL_PollEvent(&event))
+    {
+        switch (event.type) {
+            // End game
+        case SDL_QUIT:
+        case SDL_WINDOWEVENT_CLOSE:
+            g_app_status = TERMINATED;
+            break;
+
+        case SDL_KEYDOWN:
+            switch (event.key.keysym.sym) {
+            case SDLK_q:
+                // Quit the game with a keystroke
+                g_app_status = TERMINATED;
+                break;
+
+            default:
+                break;
+            }
+
+        default:
+            break;
+        }
+    }
+
+    const Uint8* key_state = SDL_GetKeyboardState(NULL);
+
+    if (key_state[SDL_SCANCODE_LEFT])
+        applied_accel.x -= 15.0f;
+    if (key_state[SDL_SCANCODE_RIGHT]) 
+        applied_accel.x += 15.0f;
+    if (key_state[SDL_SCANCODE_UP])
+        applied_accel.y -= 15.0f;
+    if (key_state[SDL_SCANCODE_DOWN]) 
+        applied_accel.y += 15.0f;
+
+    g_game_state.player->set_acceleration(applied_accel);
+
+    // This makes sure that the player can't move faster diagonally
+    //if (glm::length(g_game_state.player->get_movement()) > 1.0f)
+}
+
+void update()
+{
+    // ————— DELTA TIME ————— //
+    float ticks = (float)SDL_GetTicks() / MILLISECONDS_IN_SECOND;
+    float delta_time = ticks - g_previous_ticks;
+    g_previous_ticks = ticks;
+
+    // ————— FIXED TIMESTEP ————— //
+    // STEP 1: Keep track of how much time has passed since last step
+    delta_time += g_time_accumulator;
+
+    // STEP 2: Accumulate the ammount of time passed while we're under our fixed timestep
+    if (delta_time < FIXED_TIMESTEP)
+    {
+        g_time_accumulator = delta_time;
+        return;
+    }
+
+    // STEP 3: Once we exceed our fixed timestep, apply that elapsed time into the 
+    //         objects' update function invocation
+    while (delta_time >= FIXED_TIMESTEP)
+    {
+        g_game_state.player->update(FIXED_TIMESTEP);
+
+        delta_time -= FIXED_TIMESTEP;
+    }
+
+    g_time_accumulator = delta_time;
+}
+
+void render()
+{
+    // ————— GENERAL ————— //
+    glClear(GL_COLOR_BUFFER_BIT);
+
+    glEnableVertexAttribArray(g_shader_program.get_position_attribute());
+    glEnableVertexAttribArray(g_shader_program.get_tex_coordinate_attribute());
+
+    // ————— PLAYER ————— //
+    g_game_state.player->render(&g_shader_program);
+
+    // ————— PLATFORM ————— //
+
+    // ————— GENERAL ————— //
+    glDisableVertexAttribArray(g_shader_program.get_position_attribute());
+    glDisableVertexAttribArray(g_shader_program.get_tex_coordinate_attribute());
+
+    SDL_GL_SwapWindow(g_display_window);
+}
+
+void shutdown() 
+{
+    SDL_Quit();
+}
+
+int main(int argc, char* argv[])
+{
+    initialise();
+
+    while (g_app_status == RUNNING)
+    {
+        process_input();
+        update();
+        render();
+    }
+
+    shutdown();
+    return 0;
+}
